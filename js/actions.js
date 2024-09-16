176fd0af7cd13acc0b928206225f47a1b8f13ed1
ola pacia <olusiaop05@gmail.com>
2024-09-16 16:03:15 +0200

    reveive operational patterns in console

--- js/actions.js
+++ js/actions.js
@@ -1,14 +1,19 @@
-const calculator = document.querySelector('.calculatormain');
-const keys = calculator.querySelector(
-    '.calculator-grid-row1'
-);
+const inputValue = document.getElementById("outcome-nav");
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-        console.log('it is a number!');
-    };
-
+const number = document.querySelectorAll(".numbers").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        if (inputValue.innerText === "NaN") {
+            inputValue.innerTexr = "";
+        }
+        if (inputValue.innerText === "0") {
+            inputValue.innerText = ""
+        }
+        inputValue.innerTexr += e.target.trim();
+    });
 });
 
-
-
+const calculate = document.querySelectorAll(".operations").forEach(function (item) {
+    item.addEventListener("click", function (e) {
+        console.log(e.target.innerHTML);
+    });
+});
