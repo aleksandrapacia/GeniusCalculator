0ca64d609f53f318a38667a0e9d00ec3f4548e1a
ola pacia <olusiaop05@gmail.com>
2024-09-16 11:07:45 +0200

    text in outcome nav

---
+++ js/actions.js
@@ -0,0 +1,10 @@
+const buttonOne = document.querySelector(".button1");
+buttonOne.addEventListener("click", function () {
+    alert("1");
+    console.log('Hello Button 1')
+});
+
+const buttonAdd = document.querySelector(".button-add");
+buttonAdd.addEventListener("click", function () {
+    alert("+");
+});
